522782eb7568376e94126a21c2f3cbcfdf751e66
alejandrotoledoweb <atoledofr@gmail.com>
2021-03-22 14:48:40 -0500

    add components folder and add App.js inside

---
+++ src/components/component-2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Comp2 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      // something
+    )
+  }
+
+}
---
+++ src/components/component-4.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Comp4 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      // something
+    )
+  }
+
+}
