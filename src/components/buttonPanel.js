533c80c29b42df0d7c185e0a0548a411b74834d9
alejandrotoledoweb <atoledofr@gmail.com>
2021-03-25 12:57:41 -0500

    give style as the calculator of example

--- src/components/buttonPanel.js
+++ src/components/buttonPanel.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './button';
 
+import '../styles/buttonPanel.css';
 import '../index.css';
 
 const ButtonPanel = ({ clickHandler }) => {
